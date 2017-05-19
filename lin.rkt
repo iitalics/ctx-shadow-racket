763f3a158e5ed0e5cee846e89ae2cd6dfe59a2eb
Milo Turner <iitalics@gmail.com>
2017-05-19 15:17:42 +0000

    linear typing WITHOUT continuation / context shadowing

--- lin.rkt
+++ lin.rkt
@@ -9,70 +9,85 @@
 (define-type-constructor × #:arity = 2)
 (define-type-constructor Lin #:arity = 1)
 
-(define-type-constructor L* #:arity = 2)
-(define-type-constructor L& #:arity = 2)
-(define-type-constructor LVar  #:arity = 1)
-(define-type-constructor LVarI #:arity = 1)
-(define-base-type L1)
-(define-base-type L0)
+(define-base-type LUnit)
+(define-type-constructor L× #:arity > 0)
+(define-type-constructor L& #:arity > 0)
+(define-type-constructor LVar #:arity = 1)
+(define-base-type UP)
+(define-base-type DOWN)
 
 (begin-for-syntax
   (require syntax/parse)
 
+  (define (put-props s . args)
+    (let trav ([s s] [l args])
+      (match l
+        [(list* k v args-) (trav (syntax-property s k v) args-)]
+        ['() s]
+        [_ (error "wrong # of arguments")])))
 
-  #|
-
-  . ⊢ (a + ~b) * (b + ~a)    => OK
-
-  |#
+  (define (get-prop s k)
+    (syntax-property s k))
 
 
-  (define l-eval (current-type-eval))
 
-  (define (verify l ctx)
-    (printf "verify: ~a ⊢ ~a\n" (map type->str ctx) (type->str l))
-    (syntax-parse l
-      ; * (then)
-      [(~L* ~L1 b)
-       (verify #'b ctx)]
-      [(~L* x b) #:when (or (LVar? #'x)
-                            (LVarI? #'x))
-       (verify #'b (cons #'x ctx))]
-      [(~L* (~L& a b) c)
-       (and (verify (l-eval #'(L* a c)) ctx)
-            (verify (l-eval #'(L* b c)) ctx))]
-      [(~L* (~L* a b) c)
-       (verify (l-eval #'(L* a (L* b c))) ctx)]
 
-      ; * (and)
-      [(~L& a b)
-       (and (verify #'a ctx)
-            (verify #'b ctx))]
+  (define (mk-lvar orig)
+    (put-props ((current-type-eval) #'(LVar UP))
+               '#%lin-orig (syntax-local-introduce orig)
+               '#%lin-uniq (gensym)))
 
-      ; var
-      [(~or (~LVar _) (~LVarI _))
-       (verify (l-eval #'L1) (cons l ctx))]
+  (define (invol x)
+    (syntax-parse x
+      [(~LVar ~UP)
+       (put-props ((current-type-eval) (syntax/loc x (LVar DOWN)))
+                  '#%lin-orig (get-prop x '#%lin-orig)
+                  '#%lin-uniq (get-prop x '#%lin-uniq))]
+      [(~LVar ~DOWN)
+       (put-props ((current-type-eval) (syntax/loc x (LVar UP)))
+                  '#%lin-orig (get-prop x '#%lin-orig)
+                  '#%lin-uniq (get-prop x '#%lin-uniq))]
+      [_ x]))
 
-      ; unit
-      [~L1 (balanced? ctx)]
-      [~L0 #f]
 
-      [_ (error (format "bad form: ~a"
-                        (type->str l)))]))
 
-  (define (balanced? ctx)
-    (cond
-      [(null? ctx) #t]
-      [else
-       (let ([p (lambda (l)
-                  (syntax-parse (list (car ctx) l)
-                    [(~or ((~LVar x) (~LVarI y))
-                          ((~LVarI x) (~LVar y)))
-                     (printf "~a vs ~a\n" #'x #'y)
-                     (free-identifier=? #'x #'y)]
-                    [_ #f]))])
-         (and (ormap p (cdr ctx))
-              (balanced? (remf p ctx))))]))
+  (define (check-well-formed l)
+    (define (balance var ctx)
+      ; assuming that UP gets added first, followed by successive DOWNs
+      (syntax-parse var
+        [(~LVar ~UP)
+         (hash-set ctx (get-prop var '#%lin-uniq) var)]
+        [(~LVar ~DOWN)
+         (unless (hash-has-key? ctx (get-prop var '#%lin-uniq))
+           (raise-syntax-error #f (format "linear variable ~a used more than once"
+                                          (syntax-e (get-prop var '#%lin-orig)))
+                               (get-prop var '#%lin-orig)))
+         (hash-remove ctx (get-prop var '#%lin-uniq))]))
+
+    (define (finished ctx)
+      (for ([var (in-hash-values ctx)])
+        (raise-syntax-error #f (format "linear variable ~a unused"
+                                       (syntax-e (get-prop var '#%lin-orig)))
+                            (get-prop var '#%lin-orig))))
+
+    (let loop ([ctx (hash)]
+               [exprs (list l)])
+      (match exprs
+        ['() (finished ctx)]
+        [(cons e exprs-)
+         (syntax-parse e
+           [(~LVar _)
+            (loop (balance e ctx) exprs-)]
+
+           [(~L× l ...)
+            (loop ctx (append (syntax-e #'(l ...)) exprs-))]
+
+           [(~L& l ...)
+            (for ([l (in-list (syntax-e #'(l ...)))])
+              (loop ctx (cons l exprs-)))]
+
+           [~LUnit (loop ctx exprs-)])])))
+
 
   )
 
@@ -80,43 +95,86 @@
 (define-typed-syntax ty/datum
   [(_ . k:integer) ≫
    --------
-   [⊢ 'k (⇒ : Int) (⇒ lin L1)]]
+   [⊢ 'k (⇒ : Int) (⇒ ~ LUnit)]]
   [(_ . k:boolean) ≫
    --------
-   [⊢ 'k (⇒ : Bool) (⇒ lin L1)]]
+   [⊢ 'k (⇒ : Bool) (⇒ ~ LUnit)]]
   )
 
 
+(define-typed-syntax ty/begin
+  [(_ e ... ef) ≫
+   [⊢ e ≫ e- (⇒ : _) (⇒ ~ l)] ...
+   [⊢ ef ≫ ef- (⇒ : σ) (⇒ ~ lf)]
+   --------
+   [⊢ (begin- e- ... ef-)
+      (⇒ : σ)
+      (⇒ ~ (L× l ... lf))]])
+
+
+(define-typed-syntax ty/box
+  [(_ e) ≫
+   [⊢ e ≫ e- (⇒ : τ) (⇒ ~ l)]
+   --------
+   [⊢ (#%app- box e-) (⇒ : (Lin τ)) (⇒ ~ l)]])
+
+
 (define-typed-syntax ty/let
-  [(_ (x:id rhs) e) ≫
-   [⊢ rhs ≫ rhs- (⇒ : τ) (⇒ lin l1)]
-   #:with x* (mk-type #'x)
-   [[x ≫ x-
-         : τ
-         lin (LVar x*)]
-    ⊢ e ≫ e- (⇒ : σ) (⇒ lin l2)]
-   #:with x-* (mk-type #'x-)
+  [(_ (x rhs) e) ≫
+   [⊢ rhs ≫ rhs- (⇒ : τ) (⇒ ~ l1)]
+   #:with l/x (if (Lin? #'τ)
+                  (mk-lvar #'x)
+                  #'LUnit)
+   #:with l/x/inv (invol #'l/x)
+   [[x ≫ x- : τ ~ l/x/inv] ⊢ e ≫ e- (⇒ : σ) (⇒ ~ l2)]
    --------
    [⊢ (let- ([x- rhs-]) e-)
       (⇒ : σ)
-      (⇒ lin (L* (L* l1 l2)
-                 (LVarI x-*)))]])
+      (⇒ ~ (L× l/x l1 l2))]])
+
+
+(define-typed-syntax ty/if
+  [(_ c e1 e2) ≫
+   [⊢ c ≫ c- (⇒ : τ) (⇒ ~ l1)]
+   #:fail-unless (Bool? #'τ) "condition to 'if' expression must be Bool type"
+   [⊢ e1 ≫ e1- (⇒ : σ1) (⇒ ~ l2)]
+   [⊢ e2 ≫ e2- (⇒ : σ2) (⇒ ~ l3)]
+   #:fail-unless (type=? #'σ1 #'σ2)
+   (format "branches in condition have conflicting types ~a and ~a"
+           (type->str #'σ1)
+           (type->str #'σ2))
+   --------
+   [⊢ (if- c- e1- e2-)
+      (⇒ : σ1)
+      (⇒ ~ (L× l1 (L& l2 l3)))]])
+
+
+(define-typed-syntax ty/tup
+  [(_ e1 e2) ≫
+   [⊢ e1 ≫ e1- (⇒ : τ) (⇒ ~ l1)]
+   [⊢ e2 ≫ e2- (⇒ : σ) (⇒ ~ l2)]
+   --------
+   [⊢ (#%app- list e1- e2-)
+      (⇒ : #,(if (or (Lin? #'τ) (Lin? #'σ))
+                 #'(Lin (× τ σ))
+                 #'(× τ σ)))
+      (⇒ ~ (L× l1 l2))]])
+
+
 
 
 (define-typed-syntax (top-interact . e) ≫
   [⊢ e ≫ e-
          (⇒ : τ)
-         (⇒ lin l)]
+         (⇒ ~ l)]
+  #:do [(check-well-formed #'l)]
   #:with t->s (type->str #'τ)
-  #:with l->s (type->str #'l)
+  ;#:with l->s (type->str #'l)
   --------
-  [≻ (#%app- printf "~a : ~a\nlinear: ~a => ~a\n"
+  [≻ (#%app- printf "~a : ~a\n"
              e-
-             't->s
-             'l->s
-             '#,(if (verify #'l '())
-                    "ok"
-                    "FAILED"))])
+             't->s)])
+
 
 
 (define-syntax mod-begin
@@ -126,7 +184,10 @@
 
 (provide (type-out Int Bool ×)
          (rename-out [ty/datum #%datum]
+                     [ty/box box]
                      [ty/let let]
-                     #;[ty/tup tup]
+                     [ty/if if]
+                     [ty/begin begin]
+                     [ty/tup tup]
                      [top-interact #%top-interaction]
                      [mod-begin #%module-begin]))
