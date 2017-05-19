384e2f67b22c6f438f999b171027e47a203c17d5
Milo Turner <iitalics@gmail.com>
2017-05-19 17:24:20 +0000

    more tests

--- lin-test.rkt
+++ lin-test.rkt
@@ -1,18 +1,40 @@
 #lang s-exp "lin-no-shadow.rkt"
+; TODO: typechecking lib
 
 (let (x (box 3))
   (tup x #t))
 
-(lambda (x : (Lin Int))
+(λ (x : (Lin Int))
   (if #t
       x
       (begin x (box 0))))
 
 ; FAILS
 #;(lambda (x : (Lin Int))
-  (lambda-once (y : (Lin Int))
+    (lambda (y : (Lin Int))
+      (tup x y)))
+
+; OK:
+(λ (x : (Lin Int))
+  (λ-once (y : (Lin Int))
     (tup x y)))
 
-(lambda (x : (Lin Int))
-  (lambda-once (y : (Lin Int))
-    (tup x y)))
+
+; FAILS
+#;(let (x (box 3))
+    (begin
+      (λ (u : Unit) x)
+      x))
+
+#;(let (x (box 3))
+    (begin
+      (λ (u : Unit) x)))
+
+#;(let (x (box 3))
+    (begin
+      (λ-once (u : Unit) x)
+      x))
+
+(let (x (box 3))
+  (begin
+    (λ-once (u : Unit) x)))
